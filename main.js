9667a44d5bd5c17f14dbcf2e259c609f285c5a9b
blazecodechangers <40677474+blazecodechangers@users.noreply.github.com>
2018-08-08 13:52:12 -0600

    cookie upgrade

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var cookies = 0;
 
-function cookieClick(number){
-    cookies = cookies + number;
+function cookieClick(){
+    cookies = cookies + cUpgrade + 1;
     document.getElementById("cookies").innerHTML = cookies;
 };
 
@@ -17,6 +17,19 @@ function buyCursor(){
     };
     var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
     document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+};
+
+var cUp = 0;
+function buycUp(){
+    var cUpCost = Math.floor(25 * Math.pow(1.1,cUp));     //works out the cost of this cursor
+    if(cookies >= cUpCost){                                   //checks that the player can afford the cursor
+        cUp = cUp + 1;                                   //increases number of cursors
+    	cookies = cookies - cUp;                          //removes the cookies spent
+        document.getElementById('cUp').innerHTML = cUp;  //updates the number of cursors for the user
+        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
+    };
+    var nextCostcUp = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
+    document.getElementById('cUpCost').innerHTML = nextCostcUp;  //updates the cursor cost for the user
 };
 
 window.setInterval(function(){
