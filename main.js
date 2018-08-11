7348fd901be34579319f23c089f8284eebe91f09
blazecodechangers <40677474+blazecodechangers@users.noreply.github.com>
2018-08-10 18:19:34 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var cookies = 0;
-
+var t2 = 0;
 function cookieClick(){
     cookies = cookies + cUp + 1;
     document.getElementById("cookies").innerHTML = cookies;
 };
 
-function cookieClick2(n){
+function cookieClick1(n){
     cookies = cookies + n;
     document.getElementById("cookies").innerHTML = cookies;
 };
@@ -26,14 +26,14 @@ function buyCursor(){
 
 var cUp = 0;
 function buycUp(){
-    var cUpCost = Math.floor(25 * Math.pow(1.1,cUp));     //works out the cost of this cursor
+    var cUpCost = Math.floor(30 * Math.pow(2,cUp));     //works out the cost of this cursor
     if(cookies >= cUpCost){                                   //checks that the player can afford the cursor
         cUp = cUp + 1;                                   //increases number of cursors
     	cookies = cookies - cUpCost;                          //removes the cookies spent
         document.getElementById('cUp').innerHTML = cUp;  //updates the number of cursors for the user
         document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
     };
-    var nextCostcUp = Math.floor(30 * Math.pow(1.1,cUp));       //works out the cost of the next cursor
+    var nextCostcUp = Math.floor(30 * Math.pow(2,cUp));       //works out the cost of the next cursor
     document.getElementById('cUpCost').innerHTML = nextCostcUp;  //updates the cursor cost for the user
 };
 
@@ -50,9 +50,20 @@ function buyupgradeC(){
     document.getElementById('uCost').innerHTML = nextCostc;  //updates the cursor cost for the user
 };
 
+up1 = 1;
+function up1(){
+    var up1Cost = 1000/up1;     
+    if(cookies >= up1Cost){                                  
+        t2 = t2 + 1;                                   
+    	cookies = cookies - up1Cost;                         
+        document.getElementById('t2').innerHTML = t2;  
+        document.getElementById('cookies').innerHTML = cookies;  
+    };
+};
+
 window.setInterval(function(){
 	
-	cookieClick2(cursors * (upgradeC + 1));
+	cookieClick1(cursors * (upgradeC + 1));
 	
 }, 1000);
 
